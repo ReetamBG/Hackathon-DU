80436f7ab17c5fe015974a1f1c64f8e81580e03a
ReetamBG <r33t4m@gmail.com>
2024-10-28 14:17:13 +0530

    s

--- server/database.py
+++ server/database.py
@@ -2,6 +2,12 @@ import mysql.connector
 import sys
 from mysql.connector import Error
 
+'''
+database structure :
+1. users table - user_id (primary auto inc), name, email, password, user_type
+2. tests table - test_id (primary auto inc), test_name, test_content (questions and answers in json), 
+'''
+
 class DBHelper:
 
     def __init__(self):
@@ -19,30 +25,50 @@ class DBHelper:
     def signup(self, name, password, email, accountType):
         try:
             query = """
-            INSERT INTO users (user_id, name, email, password, user_type)
-            VALUES (NULL, %s, %s, %s, %s)
-            """
+        INSERT INTO users (user_id, name, email, password, user_type)
+        VALUES (NULL, %s, %s, %s, %s)
+        """
             self.cursor.execute(query, (name, email, password, accountType))
             self.conn.commit()
             print("User added successfully")
+
+            # Return newly created user information
+            query = "SELECT * FROM users WHERE email=%s AND password=%s"
+            self.cursor.execute(query, (email, password))
+            query_response = self.cursor.fetchone()
+            user_data = {
+                'user_id': query_response[0],
+                'user_name': query_response[1],
+                'email': query_response[2],
+                'password': query_response[3],
+                'acc_type': query_response[4]
+            }
+            return user_data
         except Error as e:
             print(f"An error occurred: {e}")
             self.conn.rollback()
+            return None
 
     def login(self, email, password):
         try:
-            query = """
-            SELECT * FROM users WHERE email=%s && password=%s
-            """
+            query = "SELECT * FROM users WHERE email=%s AND password=%s"
             self.cursor.execute(query, (email, password))
             query_response = self.cursor.fetchone()
-            if query_response is None:    # i.e if no users found
-                return -1
+            if query_response:
+                user_data = {
+                    'user_id': query_response[0],
+                    'user_name': query_response[1],
+                    'email': query_response[2],
+                    'password': query_response[3],
+                    'acc_type': query_response[4]
+                }
+                return user_data
             else:
-                return 1
+                return -1
         except Error as e:
             print(f"An error occurred: {e}")
             self.conn.rollback()
+            return None
 
     def close_connection(self):
         self.cursor.close()
